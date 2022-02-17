01061b80fa6d296b08528270b267f0f6383425d7
Liam Goss <50380089+liamgoss@users.noreply.github.com>
2022-02-16 23:09:48 -0800

    speech recognition PoC

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from re import T
 import speech_recognition as sr
 import pyttsx3, datetime, wikipedia, webbrowser, os, time, subprocess, wolframalpha, json, requests
 #https://towardsdatascience.com/how-to-build-your-own-ai-personal-assistant-using-python-f57247b4494b
@@ -9,11 +10,58 @@ sapi5 - SAPI5 on Windows
 nsss - NSSpeechSynthesizer on Mac OS X
 espeak - eSpeak on every other platform
 '''
-voices=engine.getProperty('voices')
-engine.setProperty('voice','voices[0].id')
+
+# Microsoft Voice IDs
+# Male:     HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0      => voices[0].id
+# Female:   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0       => voices[1].id
+
+
+# Define voice's properties
+engine.setProperty('voice', engine.getProperty('voices')[1].id)
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+    engine.stop() # This can be removed I think
 
-speak("Testing testing 1 2 3")
+
+
+
+
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
+        speak("Hello, good morning!")
+    elif hour >= 12 and hour < 18:
+        speak("Hello, good afternoon!")
+    else:
+        speak("Hello, good evening!")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+
+        try:
+            statement = r.recognize_google(audio, language='en-in')
+            print(f"You said: {statement}\n")
+            speak(f"You said: {statement}")
+
+        except Exception as e:
+            speak("I didn't get that, please try again")
+            return "None"
+        return statement
+
+
+# https://stackoverflow.com/questions/51992375/how-to-fix-installation-issues-for-pyaudio-portaudio-fatal-error-c1083-canno
+
+
+if __name__ == '__main__':
+    while True:
+        speak("Say something!")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
